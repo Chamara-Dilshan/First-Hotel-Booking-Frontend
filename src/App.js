423d76053995cdd17a1f0ee1b79b158c78789dc5
Chamara Dilshan <chamaradilshan0011@gmail.com>
2023-11-29 18:47:26 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Booking_hotel from "./pages/Booking_hotel/Booking_hotel";
+//import ReviewPage from "./pages/ReviewPage/ReviewPage";
+function App() {
+  return (
+    <div className="App" style={{backgroundColor: "#efefef"}}>
+    <Booking_hotel/>
+    </div>
+  );
+}
+
+export default App;
